69bdb1278a476bbdbc8c7f5a79606820d1032b83
yeon94 <hjy9946@naver.com>
2020-10-04 18:35:41 +0900

    commit

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components/native";
 import Input from "../../components/Search/Input";
 import ScrollContainer from "../../components/ScrollContainer";
 import HorizontalSlider from "../../components/HorizontalSlider";
